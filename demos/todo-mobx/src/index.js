025fd4c7c1150003053821401ddef23ac759c782
Hunter-Gu <1737087925@qq.com>
2017-05-22 10:31:31 +0800

    todo mobx

--- demos/todo-mobx/src/index.js
+++ demos/todo-mobx/src/index.js
@@ -4,6 +4,8 @@ import App from './components/app.jsx'
 import Store from './store/store.js'
 
 import './style/index.scss'
+import './style/addtodo.scss'
+import './style/todolist.scss'
 
 let store = new Store()
 
